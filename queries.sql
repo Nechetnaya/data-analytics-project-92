5efb2b99750aea644db31af8ec617155301e2167
Irina Nechetnaya <nechetnaya@gmail.com>
2023-09-05 20:59:11 +0300

    add day_of_the_week_income

--- queries.sql
+++ queries.sql
@@ -27,3 +27,29 @@ with avg_income as (
 )
 select * from avg_income where average_income < (
 	select avg(average_income) from avg_income); -- продавцы со средней выручкой сделки ниже, чем средняя среди всех продавцов
+
+select
+	name,
+	case
+		when weekday_n = '1' then 'monday'
+		when weekday_n = '2' then 'tuesday'
+		when weekday_n = '3' then 'wednesday'
+		when weekday_n = '4' then 'thursday'
+		when weekday_n = '5' then 'friday'
+		when weekday_n = '6' then 'saturday'
+		when weekday_n = '7' then 'sunday'
+	end as weekday,
+	income
+from (
+	select
+		concat(e.first_name, e.last_name) as name,
+		to_char(s.sale_date, 'id') as weekday_n,
+		floor(sum(s.quantity*p.price)) as income
+	from employees e
+	left join sales s
+	on s.sales_person_id = e.employee_id
+	left join products p
+	on p.product_id = s.product_id
+	group by concat(e.first_name, e.last_name), weekday_n
+	order by weekday_n, name
+) as result; -- прибыль по продовцам по дням недели
